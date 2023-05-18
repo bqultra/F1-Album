8bd2af5be3d9595d4b2681e6d7b78b7ffdd4a31f
BQ ULTRA ⚡️ <bqultra@gmail.com>
2023-05-18 18:49:42 +0200

    Added button functionality + testing

--- F1 Album/F1 Album/ViewController.swift
+++ F1 Album/F1 Album/ViewController.swift
@@ -16,23 +16,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var carImage: UIImageView!
     
+    @IBOutlet weak var lightsOutBtn: UIButton!
     
+    let teamLogos = ["Scuderia Ferrari": "SF"]
+    
+    let ferrariCars = [
+        ["F1-75", "2022"]
+    ]
     
     @IBAction func pickTeam(_ sender: UIButton) {
         
-        
+        construtorLogo.image = UIImage(named: teamLogos["Scuderia Ferrari"]!)
         
     }
     
     @IBAction func ctaButton(_ sender: UIButton) {
         
-        
+        carImage.image = UIImage(named: ferrariCars[0][0])
+        chassisLabel.text = ferrariCars[0][0]
+        seasonLabel.text = ferrariCars[0][1]
         
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
     }
 
