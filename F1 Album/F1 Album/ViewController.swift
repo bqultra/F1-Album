9ef2825e898bed63c73097e3659c43a84a2189e5
BQ ULTRA ⚡️ <bqultra@gmail.com>
2023-05-18 16:40:57 +0200

    Setup and constraints done
    Added two photos

---
+++ F1 Album/F1 Album/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  F1 Album
+//
+//  Created by Szymek on 17/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var construtorLogo: UIImageView!
+    
+    @IBOutlet weak var chassisLabel: UILabel!
+    @IBOutlet weak var seasonLabel: UILabel!
+    
+    @IBOutlet weak var carImage: UIImageView!
+    
+    
+    
+    @IBAction func pickTeam(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func ctaButton(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
